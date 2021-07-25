17af128b33b08ec3b1fe97f8f262221c5ed233bb
Luciano Riente <lucianotomasriente@gmail.com>
2021-07-25 19:50:29 -0300

    WebApp funcionando , el mejor producto es elegido por el precio mas barato

--- js/main.js
+++ js/main.js
@@ -20,7 +20,6 @@ const productPrice = $('.product__price');
 const productInstallments = $('.product__installments');
 const productDescriptionParagraph = $('.product__description-paragraph');
 
-let finalResult; /* Search final result */
 let description; /* Description of the product */
 let title; /* Title of the product */
 let link; /* Link of the product */
@@ -35,43 +34,57 @@ inputBrowser.addEventListener('input', (e) => {
 
 buttonForm.addEventListener('click', (e) => {
 	e.preventDefault();
-    productInstallments.innerText = '';
 	if (userQuery !== undefined && userQuery !== '') {
-		console.log(userQuery);
 		showResults();
 	}
 	inputBrowser.value = '';
 });
 
+let prices = []; /* All the product's prices founded */
+let bestPrice; /* The best price of the product's prices founded */
+let results; /* Products coincidences with the user query */
+let bestProductByPrice; /* The cheapest product */
+
 async function showResults() {
 	await fetch(`https://api.mercadolibre.com/sites/MLA/search?q=${userQuery}`)
 		.then((response) => response.json())
 		.then((json) => {
-			let results = json.results;
-			// results.forEach((result) => {
-			// 	console.log(result.title);
-			// });
+			results = json.results;
+			if (results.length === 0) {
+				alert('No se han encontrado resultados');
+				productInformation.style.display = 'none';
+				return false;
+			}
+			for (const result of results) {
+				prices.push(result.price);
+			}
+			prices.sort((a, b) => {
+				return a - b;
+			});
 			for (let i = 0; i < results.length; i++) {
-				finalResult = results[0];
-				title = finalResult.title;
-				link = finalResult.permalink;
-				image = finalResult.thumbnail;
-				price = finalResult.price;
-				if (finalResult.installments !== null) {
-					installmentsAmount = `En ${finalResult.installments.quantity} cuotas de $${finalResult.installments.amount}`;
-					productInstallments.innerText = `${installmentsAmount}`;
+				bestPrice = results[i].price;
+				if (bestPrice === prices[0]) {
+					bestProductByPrice = results[i];
+					title = bestProductByPrice.title;
+					link = bestProductByPrice.permalink;
+					image = bestProductByPrice.thumbnail;
+					price = bestProductByPrice.price;
 				}
 			}
-			console.log(finalResult);
-			console.log(title, link, image, price, installmentsAmount);
-		});
-	await fetch(`https://api.mercadolibre.com/items/${finalResult.id}/description`)
+		})
+		.catch((error) => console.error(error));
+
+	await fetch(`https://api.mercadolibre.com/items/${bestProductByPrice.id}/description`)
 		.then((response) => response.json())
 		.then((json) => {
+			if (results.length === 0) { /* Ver alguna manera para no poner esto otra vez y tirar directamente un error que no ejecute más el código (?) */
+				return false;
+			}
 			description = json.plain_text;
-			console.log(description);
-		});
-	drawTheBestProduct();
+			description !== '' ? (description = json.plain_text) : (description = 'El vendedor no colocó ninguna descripción');
+			drawTheBestProduct();
+		})
+		.catch((error) => console.error(error));
 }
 
 function drawTheBestProduct() {
@@ -80,5 +93,29 @@ function drawTheBestProduct() {
 	productImage.src = `${image}`;
 	productTitle.innerText = `${title}`;
 	productPrice.innerText = `$${price}`;
+	if (bestProductByPrice.installments !== null) {
+		installmentsAmount = `En ${bestProductByPrice.installments.quantity} cuotas de $${bestProductByPrice.installments.amount}`;
+		productInstallments.innerText = `${installmentsAmount}`;
+	}
 	productDescriptionParagraph.innerText = `${description}`;
+	prices = [];
 }
+
+// Se eligió el mejor producto dependiendo si era el más barato
+
+// function noCoincidencesFunction() {
+// 	productInformation.style.display = 'none';
+// 	title = '';
+// 	link = '';
+// 	image = '';
+// 	price = '';
+// 	installmentsAmount = '';
+// 	description = '';
+// 	productImageLink.href = `${link}`;
+// 	productImage.src = `${image}`;
+// 	productTitle.innerText = `${title}`;
+// 	productPrice.innerText = `$${price}`;
+// 	productInstallments.innerText = `${installmentsAmount}`;
+// 	console.log(title, link, image, price, installmentsAmount, description);
+// }
+
